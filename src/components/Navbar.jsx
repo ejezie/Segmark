55c913d83e0fb58350f7ca492f4e1078d86a5fd9
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-03-30 14:01:58 +0100

    included logo and burger menu at navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,20 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
+
+import logo from "../assets/icons/logo.png";
+import menu from "../assets/icons/menu.png";
 
 function Navbar({ handleMenuClick, }) {
   return (
     <div className="navbar center">
       <div className="logo center">
-        <div className="logo-icon">LOGO</div>
+        <div className="logo-icon">
+          <Link to={"/"}>
+            <img src={logo} alt="logo" />
+          </Link>
+        </div>
         <div className="bar" onClick={handleMenuClick}>
-          Burgr
+          <img src={menu} alt="menu" />
         </div>
       </div>
       <div className={`links center`}>
