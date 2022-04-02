8f98f6107c5d72141f39ea491d6c05d515e61934
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-04-02 16:49:41 +0100

    adding animation to about page

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,19 +1,30 @@
 import React from "react";
 
+import { useInView } from "react-hook-inview";
+
 import openImage from "../assets/icons/open.png";
 import closeImage from "../assets/icons/close.png";
 
 function Question({ textQ, answer }) {
   const [open, setOpen] = React.useState(false);
+  const [qObserve, inViewQ] = useInView({
+    threshold: "0.5",
+  });
 
   return (
-    <div className={`question ${!open && "q-height"}`}>
+    <div
+      ref={qObserve}
+      className={`question opaczro ${!open && "q-height"} ${inViewQ && "opacone move-up"}`}
+      style={{ animationDelay: "0.8s", transitionDelay: "0.8s" }}
+    >
       <div className="question-text-wrap center">
         <div className="question-text">{textQ}</div>
         <div className="icon center">
           <img
             className="img"
-            onClick={() => {setOpen(!open)}}
+            onClick={() => {
+              setOpen(!open);
+            }}
             src={open ? closeImage : openImage}
             alt="icon"
           />
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,23 +2,39 @@ import React from "react";
 
 import Info from "../components/Info";
 
+import { useInView } from "react-hook-inview";
+
 import ygirl from "../assets/images/ygirl.png";
-import hans from "../assets/images/hans.png";
-import infobg from "../assets/images/infobg.png";
-import kite from "../assets/images/kite.png";
-import bulb from "../assets/images/bulb.png";
 
 import Question from "../components/Question";
 
 function About() {
   const data = require("../assets/questions.json");
-  console.log(data);
+  // console.log(data);
+
+  const [abObserve, inViewAb] = useInView();
+  const [abObserve1, inViewAb1] = useInView();
+  const [abObserve2, inViewAb2] = useInView();
+  const [abObserve3, inViewAb3] = useInView();
+  const [abObserve4, inViewAb4] = useInView();
 
   return (
     <div className="section-wrap">
       <div className="about center">
-        <div className="sub-heading sub">About Segmak</div>
-        <div className="text txt">
+        <div
+          ref={abObserve}
+          className={` sub-heading sub opaczro ${
+            inViewAb && "opacone move-up"
+          }`}
+          style={{ animationDelay: "0.2s", transitionDelay: "0.2s" }}
+        >
+          About Segmak
+        </div>
+        <div
+          ref={abObserve1}
+          className={` text txt opaczro ${inViewAb1 && "opacone move-up"}`}
+          style={{ animationDelay: "0.4s", transitionDelay: "0.4s" }}
+        >
           Segmak is the best school ecosystem that combines teacher, student,
           parent and other school staffs in a single automation software. It
           includes a modern design that saves time and provides better insight
@@ -26,13 +42,20 @@ function About() {
           school-wide view, and much more.
         </div>
         <div className="mission center">
-          <div className="left center">
+          <div
+            ref={abObserve2}
+            className={` left center opaczro ${inViewAb2 && "opacone move-up"}`}
+            style={{ animationDelay: "0.2s", transitionDelay: "0.2s" }}
+          >
             <img src={ygirl} alt="girl" />
             <div className="rings1"></div>
             <div className="rings2"></div>
           </div>
-          <div className="right center">
-            <div className="text-wrap">
+          <div ref={abObserve3} className="right center">
+            <div
+              className={` text-wrap opaczro ${inViewAb3 && "opacone move-up"}`}
+              style={{ animationDelay: "0.2s", transitionDelay: "0.2s" }}
+            >
               <div className="sub-heading marginb1">Mission</div>
               <div className="text" style={{ marginBottom: "4rem" }}>
                 Education in today's world should be seamless and accessible.
@@ -42,7 +65,10 @@ function About() {
                 and parents collaborate to provide high-quality education.
               </div>
             </div>
-            <div className="text-wrap">
+            <div
+              className={` text-wrap opaczro ${inViewAb3 && "opacone move-up"}`}
+              style={{ animationDelay: "0.4s", transitionDelay: "0.4s" }}
+            >
               <div className="sub-heading marginb1">Vission</div>
               <div className="text">
                 With our excellent solutions, we hope to bring about a radical
